212ffaf877d6e1d26ccfbc91338e67a88d673882
Duy <dtn9197@gmail.com>
2020-09-02 03:53:28 -0400

    commit

---
+++ scripts/math.js
@@ -0,0 +1,5 @@
+export const subtract = function(x,y) {
+    return x - y;
+}
+
+export const add = (x,y) => x + y;
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+import {add, subtract} from "./math.js";
+
+
+console.log("hello world");
+console.log(add(3,4));
+console.log(subtract(4,6));
